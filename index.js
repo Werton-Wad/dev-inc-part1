5ca99543276c9dd022b20a7e8beb2781c47c0ed4
Wad Werton <wad_werton@mail.ru>
2020-09-22 23:32:41 +0300

    feat: add some features

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const $worksPhotosContainer = document.querySelector('.works__photos');
-const $popupImage = document.querySelector('.popup-image');
-const addPopupImage = (e) => {
-    if (e.target.tagName === 'IMG') {
-        $popupImage.src = e.target.dataset.url;
-    }
-    $('#exampleModalCenter').modal('show');
-}
+const workers = [
+    { name: 'Aida Bugg', position: 'UI Designer', description: 'vcxfsfefweffffffffffffffewfwfwef fewfwef' },
+    { name: 'Al Rayhan', position: 'UX Designer', description: 'Lorem Ipsum is not simply is an action designer random text. It has roots in a piece.' },
+    { name: 'Allie Grater', position: 'Senior JS developer', description: 'dadadad eofj iofrioef mirof iofrefjio fr' },
+    { name: 'Maureen Biologist', position: 'Owner', description: 'my company and my workers live very good' },
+    { name: 'Paddy O\'Furniture', position: 'Lawyer', description: 'bla bla lba bla'},
+    { name: 'Teri Dactyl', position: 'Middle js developer', description: 'ye yey ye ye ye'},
+];
 
-
-$worksPhotosContainer.addEventListener('click', addPopupImage);
+(function (workers) {
+    const $speechBubblesTitles = document.querySelectorAll('.speech-bubble__title');
+    const $speechBubblesTexts = document.querySelectorAll('.speech-bubble__text');
+    return workers.map((worker, i) => {
+        $speechBubblesTitles[i].innerHTML = `<h5>${worker.name.toUpperCase()} <span>/ ${worker.position}</span></h5>`;
+        $speechBubblesTexts[i].innerHTML = `<p>${worker.description}<p>`;
+    })
+}(workers));
